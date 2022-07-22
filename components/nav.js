c3d744e2eb9f8af5b8b311dfc704343a451a9582
nathenjo <nateselectronicrepair@gmail.com>
2022-07-22 15:45:33 -0400

    Finished animations for nav menu

--- components/nav.js
+++ components/nav.js
@@ -1,11 +1,25 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+
+import DeleteIcon from '@mui/icons-material/Delete';
 
 export default function Nav(props) {
     const { propClassName, statePageLocation, stateSetPageLocation } = props;
 
     const [showNav, setShowNav] = useState(true);
 
-
+    useEffect(() => {  
+        document.querySelector('#nav-toggle-button').addEventListener('click', function() {
+            if (showNav) {
+                document.querySelector('#nav-toggle-button').style.left = '6%';
+                document.querySelector('nav').classList.add('show-nav');
+                document.querySelector('nav').classList.remove('hide-nav');
+            } else if(showNav == false) {
+                document.querySelector('#nav-toggle-button').style.left = '0';
+                document.querySelector('nav').classList.add('hide-nav');
+                document.querySelector('nav').classList.remove('show-nav');
+            }
+        })
+    })
 
     const setPageState = (e) => {
         e.preventDefault();
@@ -13,7 +27,7 @@ export default function Nav(props) {
     }
 
     return(
-        <div  className={`${propClassName}`}>
+        <div className={`nav-container ${propClassName}`}>
             <nav>
                 {String(statePageLocation).toLowerCase() != 'home' && <a href='/'>Home</a>}
                 <a className='button' onClick={(e) => setPageState(e)}>Teams</a>
@@ -21,7 +35,9 @@ export default function Nav(props) {
                 <a className='button' onClick={(e) => setPageState(e)}>Socials</a>
                 <a className='button' onClick={(e) => setPageState(e)}>Schedule</a>
             </nav>
-            <button onClick={() => setShowNav(!showNav)}/>
+            <button id='nav-toggle-button' className='nav-toggle' onClick={() => setShowNav(!showNav)}>
+                <svg data-testid="DeleteIcon"></svg>
+            </button>
         </div>
     )
 }
